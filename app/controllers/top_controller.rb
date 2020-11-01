8bb3ab14ebc60d1ecb6e00e5734a1de1bac6285b
ukkiy-ray <showy13f207ag@gmail.com>
2020-11-01 13:03:00 +0900

    deviseによるユーザーと企業の管理機能実装

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,13 @@
 class TopController < ApplicationController
+
+
   def index
+    if user_signed_in?
+      @companies = Company.all
+    elsif company_signed_in?
+      @users = User.all
+    end
   end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :companies, controllers: {
+    sessions:      'companies/sessions',
+    passwords:     'companies/passwords',
+    registrations: 'companies/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+
   root to: 'top#index'
 end
